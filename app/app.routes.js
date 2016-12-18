a01c3851941be512339e1d6c7de26ffce1582d2b
Liger <ngotruongphamquang@gmail.com>
2016-12-18 21:31:32 +0700

    Refresh repository to change to use angular-cli for building project

--- app/app.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var app_1 = require("./app");
-var login_component_1 = require("./login/login.component");
-exports.routes = [
-    { path: '', component: app_1.App },
-    { path: 'login', component: login_component_1.LoginComponent },
-    { path: 'signup', component: login_component_1.LoginComponent },
-    { path: '**', component: app_1.App }
-];
--- app/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-
-// The bootstrap process in the final release of Angular 2 requires a module instead of a component
-platformBrowserDynamic().bootstrapModule(AppModule);
