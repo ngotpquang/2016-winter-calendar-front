603e46af122e9114aec85bf7811babd3fbad9c0c
Vo Duc Hong <vo.duc.hong94@gmail.com>
2016-12-30 17:36:13 +0700

    implement type of end date for editting

--- src/app/edit-goal/edit-goal.component.ts
+++ src/app/edit-goal/edit-goal.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
@@ -7,22 +7,22 @@ import { Goal, Repetition, EndDate } from '../shared/goal';
 import { GoalService } from '../goal/goal.service';
 
 @Component({
-    moduleId:''+module.id,
-    selector:'app-edit-goal',
-    templateUrl:'./edit-goal.component.html',
-    styleUrls:['./edit-goal.component.scss']
+    moduleId: '' + module.id,
+    selector: 'app-edit-goal',
+    templateUrl: './edit-goal.component.html',
+    styleUrls: ['./edit-goal.component.scss']
 })
-export class EditGoalComponent implements OnInit{
+export class EditGoalComponent implements OnInit {
     id: string;
     goal: Goal;
     repetitionTypes = ['Daily', 'Weekly', 'Monthly', 'Yearly'];
-    public repetitionLimitedTimes: string[];
+    repetitionLimitedTimes = ['Forever', 'Until a date', 'For a number of events'];
     public commonFunctions: CommonFunctions;
     submitted = false;
     isDataLoaded = false;
     constructor(private goalService:GoalService, private route:ActivatedRoute, private location:Location){
     };
-    ngOnInit():void{
+    ngOnInit(): void {
         this.id = this.route.params['_value']['id'];
         this.commonFunctions = new CommonFunctions();
         this.goalService.getGoalsById(this.id).toPromise().then((data)=>{
@@ -31,22 +31,26 @@ export class EditGoalComponent implements OnInit{
             this.isDataLoaded = true;
         });
     };
-    get diagnostic() { 
-        console.log("This is my goal: " + this.goal);
-        return JSON.stringify(this.goal); 
+    get diagnostic() {
+        console.log('This is my goal: ' + this.goal);
+        return JSON.stringify(this.goal);
     }
-    log():void{
+    log(): void{
         console.log(this.goal);
     }
-    editGoal():void{
+    editGoal(): void {
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
         this.goal.token = currentUser.token;
         this.goal.email = currentUser.email;
         console.log(this.goal);
-        this.goalService.editGoal(this.id,this.goal).then(()=>window.location.reload());
+        this.goalService.editGoal(this.id, this.goal).then(() => window.location.reload());
     }
-    toNumber():void{
-        let num = parseInt(this.goal.repetition.type_of_repetition.toString(),10);
+    toNumber(): void {
+        let num = parseInt(this.goal.repetition.type_of_repetition.toString(), 10);
         this.goal.repetition.type_of_repetition = num;
+    }
+    toNumber0(): void {
+        let num = parseInt(this.goal.end_date.type_of_end_date.toString(), 10);
+        this.goal.end_date.type_of_end_date = num;
     }
 }
