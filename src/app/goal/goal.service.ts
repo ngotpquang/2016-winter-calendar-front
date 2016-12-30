a9146efd452882ffc81f11486b814a60f23147cc
Vo Duc Hong <vo.duc.hong94@gmail.com>
2016-12-30 10:40:53 +0700

    init app-edit-goal

--- src/app/goal/goal.service.ts
+++ src/app/goal/goal.service.ts
@@ -35,4 +35,16 @@ export class GoalService {
       let currentUser = JSON.parse(localStorage.getItem('currentUser'));
       return this.http.put('https://wintercalendar.herokuapp.com/api/v1/goals/' + id + '/calendar', JSON.stringify({token: currentUser.token, id: id, date_of_calendar: date_of_calendar,status: status}));
     }
+
+    editGoal(id: string, goal:Goal):Promise<Goal>{
+        let url = `https://wintercalendar.herokuapp.com/api/v1/goals/${id}`;
+        return this.http
+        .put(url,JSON.stringify(goal),{headers:headers})
+        .toPromise().then(()=>goal);
+
+    }
+    private handleError(error: any):Promise<any>{
+        console.error("An error occured", error);
+        return Promise.reject(error.message||error);
+    }
 }
