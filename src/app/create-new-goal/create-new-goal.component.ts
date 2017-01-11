f8a5d643e6d8ddd4eb03cc0017140f4888b28d21
Liger <ngotruongphamquang@gmail.com>
2017-01-11 09:19:53 +0700

    2017-Calendar: Add responsive to introduction UI

--- src/app/create-new-goal/create-new-goal.component.ts
+++ src/app/create-new-goal/create-new-goal.component.ts
@@ -139,7 +139,6 @@ export class CreateNewGoalComponent extends LoadingPage implements OnInit {
     showReminders(reminderId?: string) {
         let inputForm = this.createNewGoalForm.value;
         let numOfRemindersShowed: number = 0;
-        // console.log(reminderId);
         if (typeof (reminderId) == 'undefined') {
             let reminders = document.getElementsByClassName('reminder-group');
             let selects = document.getElementsByClassName('time-before');
@@ -147,20 +146,17 @@ export class CreateNewGoalComponent extends LoadingPage implements OnInit {
                 let element = (<HTMLElement>reminders[reminder]);
                 if (element.hidden == true) {
                     element.hidden = false;
-                    // console.log(element.getAttribute('id'));
                     this.resetInputFormControl(element.getAttribute('id'));
                     (<HTMLInputElement>selects[reminder]).value = "";
                     break;
                 }
                 numOfRemindersShowed += 1;
             }
-            // console.log(numOfRemindersShowed);
             if (numOfRemindersShowed == 4) {
                 this.hiddenReminderButton = true;
             }
         } else {
             let reminder = <HTMLElement>document.getElementById(reminderId);
-            // console.log(reminder);
             reminder.hidden = true;
             let select = <HTMLInputElement>document.getElementById('select-' + reminderId);
             let input = <HTMLInputElement>document.getElementById('input-' + reminderId);
@@ -173,24 +169,10 @@ export class CreateNewGoalComponent extends LoadingPage implements OnInit {
                     numOfRemindersShowed += 1
                 }
             }
-            // console.log(numOfRemindersShowed);
             if (numOfRemindersShowed < 10) {
                 this.hiddenReminderButton = false;
             }
         }
-    }
-
-    getStartHour() {
-        let inputForm = this.createNewGoalForm.value;
-        let start_date;
-        if (inputForm.start_date == null) {
-            start_date = this.roundUpTime();
-        } else {
-            start_date = new Date(inputForm.start_date);
-            start_date.setHours(start_date.getHours() - 7);
-
-        }
-        return start_date.getHours() + ":" + (start_date.getMinutes() == 0 ? "00" : "30");
     }
 
     changeToSetTime(item: string): void {
@@ -198,8 +180,8 @@ export class CreateNewGoalComponent extends LoadingPage implements OnInit {
         let input = <HTMLInputElement>document.getElementById('input-' + item);
         if (select.value == "7") {
             input.hidden = false;
-            console.log(this.getStartHour());
-            input.value = this.getStartHour();
+            console.log(this.startTime.split('T')[1]);
+            input.value = this.startTime.split('T')[1];
         } else {
             input.hidden = true;
         }
@@ -209,7 +191,6 @@ export class CreateNewGoalComponent extends LoadingPage implements OnInit {
         let input = this.createNewGoalForm.value;
         if (input.goal_name != "") {
             this.standby();
-            // console.log(input);
             let day_of_week = null;
             if (input.type_of_repetition == 2) {
                 day_of_week =
@@ -221,13 +202,6 @@ export class CreateNewGoalComponent extends LoadingPage implements OnInit {
                     (input.day_of_week_Fri == true ? "5, " : "") +
                     (input.day_of_week_Sat == true ? "6, " : "");
             }
-            // let start_date;
-            // if (input.start_date == null) {
-            //     start_date = this.roundUpTime().toString();
-            // } else {
-            //     start_date = input.start_date;
-            // }
-            console.log(this.startTime);
             let specific_end_date = null;
             if (input.type_of_end_date == 2) {
                 if (input.specific_end_date == null) {
@@ -255,10 +229,8 @@ export class CreateNewGoalComponent extends LoadingPage implements OnInit {
             }
             let startHours = new Date
             if (input.time_before_1 != null && timeBeforeGroup[0].hidden != true) {
-                // console.log(input.time_before_1_t);
-                // console.log(start_date);
                 if (input.time_before_1 == "7") {
-                    this.commonFunctions.pushReminder(time_before, (input.time_before_1_t == null ? this.getStartHour() : input.time_before_1_t));
+                    this.commonFunctions.pushReminder(time_before, (input.time_before_1_t == null ? this.startTime.split('T')[1] : input.time_before_1_t));
                 } else {
                     this.commonFunctions.pushReminder(time_before, this.commonFunctions.timeForReminder(input.time_before_1));
                 }
@@ -266,28 +238,28 @@ export class CreateNewGoalComponent extends LoadingPage implements OnInit {
             }
             if (input.time_before_2 != null && timeBeforeGroup[1].hidden != true) {
                 if (input.time_before_2 == "7") {
-                    this.commonFunctions.pushReminder(time_before, (input.time_before_2_t == null ? this.getStartHour() : input.time_before_2_t));
+                    this.commonFunctions.pushReminder(time_before, (input.time_before_2_t == null ? this.startTime.split('T')[1] : input.time_before_2_t));
                 } else {
                     this.commonFunctions.pushReminder(time_before, this.commonFunctions.timeForReminder(input.time_before_2));
                 }
             }
             if (input.time_before_3 != null && timeBeforeGroup[2].hidden != true) {
                 if (input.time_before_3 == "7") {
-                    this.commonFunctions.pushReminder(time_before, (input.time_before_3_t == null ? this.getStartHour() : input.time_before_3_t));
+                    this.commonFunctions.pushReminder(time_before, (input.time_before_3_t == null ? this.startTime.split('T')[1] : input.time_before_3_t));
                 } else {
                     this.commonFunctions.pushReminder(time_before, this.commonFunctions.timeForReminder(input.time_before_3));
                 }
             }
             if (input.time_before_4 != null && timeBeforeGroup[3].hidden != true) {
                 if (input.time_before_4 == "7") {
-                    this.commonFunctions.pushReminder(time_before, (input.time_before_4_t == null ? this.getStartHour() : input.time_before_4_t));
+                    this.commonFunctions.pushReminder(time_before, (input.time_before_4_t == null ? this.startTime.split('T')[1] : input.time_before_4_t));
                 } else {
                     this.commonFunctions.pushReminder(time_before, this.commonFunctions.timeForReminder(input.time_before_4));
                 }
             }
             if (input.time_before_5 != null && timeBeforeGroup[4].hidden != true) {
                 if (input.time_before_5 == "7") {
-                    this.commonFunctions.pushReminder(time_before, (input.time_before_5_t == null ? this.getStartHour() : input.time_before_5_t));
+                    this.commonFunctions.pushReminder(time_before, (input.time_before_5_t == null ? this.startTime.split('T')[1] : input.time_before_5_t));
                 } else {
                     this.commonFunctions.pushReminder(time_before, this.commonFunctions.timeForReminder(input.time_before_5));
                 }
@@ -295,13 +267,6 @@ export class CreateNewGoalComponent extends LoadingPage implements OnInit {
             if (time_before.length == 0) {
                 time_before.push("0");
             }
-            // console.log(input.time_before_1);
-            // console.log(input.time_before_2);
-            // console.log(input.time_before_3);
-            // console.log(input.time_before_4);
-            // console.log(input.time_before_5);
-            // console.log("Result: " + time_before.toString());
-
 
             let currentUser = JSON.parse(localStorage.getItem('currentUser'));
             let goal = new Goal(currentUser.email, currentUser.token, input.goal_name, this.startTime, input.description,
